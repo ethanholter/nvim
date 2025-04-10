a3c4e3b8b7b5bfa09c1c792f80f71f794453b888
Ethan Holter <holterethan@gmail.com>
2025-04-10 13:53:54 -0500

    setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim 
+sudo rm -f /usr/local/bin/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo rm -f ./nvim-linux-x86_64.tar.gz
+sudo mv /opt/nvim-linux-x86_64 /opt/nvim
+sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
