1605002fccf593eddfcd9484ddcb703aa708d100
Ethan Holter <holterethan@gmail.com>
2025-04-16 13:12:07 -0500

    added dotfiles starter pack

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 sudo rm -f ./nvim-linux-x86_64.tar.gz
 sudo mv /opt/nvim-linux-x86_64 /opt/nvim
 sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
-git clone https://github.com/ethanholter/dotfiles.nvim ~/.config/nvim
+git clone https://github.com/ethanholter/nvim ~/.config/nvim
 curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/0xProto.tar.xz
 sudo tar -xvf 0xProto.tar.xz -C /usr/local/share/fonts/
 sudo fc-cache -fv
