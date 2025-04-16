b60e40563fc822fb7f16fa0ba8032aec90fd41d4
Ethan Holter <holterethan@gmail.com>
2025-04-16 13:20:34 -0500

    install lazygit

--- setup.sh
+++ setup.sh
@@ -12,3 +12,7 @@ git clone https://github.com/ethanholter/nvim ~/.config/nvim
 curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/0xProto.tar.xz
 sudo tar -xvf 0xProto.tar.xz -C /usr/local/share/fonts/
 sudo fc-cache -fv
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
