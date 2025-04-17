3e2025b0e463c2d7d294fab03720ce5f4769861b
Ethan Holter <holterethan@gmail.com>
2025-04-16 21:20:23 -0500

    clean up setup script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,32 @@
 #! /usr/bin/env bash
+set -euo pipefail
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+temp_dir=$(mktemp -d)
+
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz -o ${temp_dir}/nvim-linux-x86_64.tar.gz
+
+# download nvim
 sudo rm -rf /opt/nvim
-sudo rm -f /usr/local/bin/nvim
-rm -rf ~/.config/nvim
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-sudo rm -f ./nvim-linux-x86_64.tar.gz
+sudo tar -C /opt -xzf ${temp_dir}/nvim-linux-x86_64.tar.gz
 sudo mv /opt/nvim-linux-x86_64 /opt/nvim
+
+# link binary to bin directory so it will be in the path without messing with the path var itself
+sudo rm -f /usr/local/bin/nvim
 sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
+
+# setup config
+rm -rf ~/.config/nvim
 git clone https://github.com/ethanholter/nvim ~/.config/nvim
-curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.tar.xz
-sudo tar -xvf 0xProto.tar.xz -C /usr/local/share/fonts/
+
+# setup nerd font
+FONT="JetBrainsMono"
+curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/${FONT}.tar.xz -o ${temp_dir}/${FONT}.tar.xz
+sudo tar -xvf ${temp_dir}/${FONT}.tar.xz -C /usr/local/share/fonts/
 sudo fc-cache -fv
+
+# setup lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/
+curl -Lo ${temp_dir}/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf ${temp_dir}/lazygit.tar.gz -C ${temp_dir}
+sudo install ${temp_dir}/lazygit -D -t /usr/local/bin/
+rm -R ${temp_dir}
